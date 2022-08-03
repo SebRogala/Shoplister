952e4f0b2eab1ea52f65f271b4458481e39a06c7
Sebastian Rogala <sebrogala@gmail.com>
2022-08-03 18:13:08 +0200

    Add very basic outline of ShoppingListId list

--- assets/views/ShoppingList/ShoppingListId.vue
+++ assets/views/ShoppingList/ShoppingListId.vue
@@ -1,5 +1,19 @@
 <template>
-    id
+    <v-row
+        class="shopping-list__row"
+        v-for="item in x"
+        dense
+    >
+        <v-col>
+            <v-sheet>
+                {{ item.name }}
+            </v-sheet>
+        </v-col>
+
+        <v-col cols="3">
+            {{ item.quantity }} {{ item.unit }}
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -7,7 +21,36 @@ export default {
     name: 'ShoppingListId',
     data() {
         return {
+            x: [
+                {
+                    name: 'Cukier',
+                    quantity: 5,
+                    unit: "kg"
+                },
+                {
+                    name: 'Szynka drobiowa',
+                    quantity: 100,
+                    unit: "g"
+                },
+                {
+                    name: 'Jakaś bardzo długa nazwa jakiegoś produktu',
+                    quantity: 67676,
+                    unit: "szt"
+                },
+                {
+                    name: 'Mleko',
+                    quantity: 2,
+                    unit: "szt"
+                },
+
+            ]
         }
     }
 }
 </script>
+
+<style>
+.shopping-list__row {
+    border-bottom: black solid 1px;
+}
+</style>
