fe7c0d7ca6c911cf2c254ed0434823bd1883bdf2
Sebastian Rogala <sebrogala@gmail.com>
2022-09-09 19:27:54 +0200

    Clear Home view

--- assets/views/Home.vue
+++ assets/views/Home.vue
@@ -2,35 +2,14 @@
 export default {
     name: 'Home',
     data() {
-        return {
-            listName: "",
-        }
     },
     methods: {
-        addList() {
-            // this.$api.post('/shopping_list', this.$api.formData({name: this.listName})).then(res => {
-            //     console.log(res);
-            // });
-        },
-        getList() {
-            // this.axios.get()
-        }
     }
 }
 </script>
 
 <template>
     <div>
-        <v-alert
-            type="success"
-        >Testing components</v-alert>
-
-        <v-text-field
-            label="List name"
-            variant="outlined"
-            v-model="listName"
-        ></v-text-field>
-
-        <v-btn @click="addList">Dodaj</v-btn>
+        Placeholder for some dashboard/statistics
     </div>
 </template>
